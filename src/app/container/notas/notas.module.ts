b65c7e6210ab4399388beab385cead1f11c7aa0f
Samuel Herrera <samuel.herrera@autozone.com>
2019-06-18 12:19:32 -0500

    added note editor

--- src/app/container/notas/notas.module.ts
+++ src/app/container/notas/notas.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotasRoutingModule } from './notas-routing.module';
+import { NotasComponent } from './notas.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotasComponent],
   imports: [
     CommonModule,
-    NotasRoutingModule
+    NotasRoutingModule,
+    EditorModule,
+    MatButtonModule
   ]
 })
 export class NotasModule { }
