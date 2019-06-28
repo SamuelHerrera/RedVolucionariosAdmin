a5b6549d2d2ca183a8069778a3759bf448dc4167
Samuel Herrera <samuelherrerafuente@gmail.com>
2019-06-28 01:49:07 -0500

    added calendar * date handlings

--- src/app/container/eventos/eventos.module.ts
+++ src/app/container/eventos/eventos.module.ts
@@ -2,12 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EventosRoutingModule } from './eventos-routing.module';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [CalendarioComponent],
   imports: [
     CommonModule,
-    EventosRoutingModule
+    FormsModule,
+    EventosRoutingModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class EventosModule { }
